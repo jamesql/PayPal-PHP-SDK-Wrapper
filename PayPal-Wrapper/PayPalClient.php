7b83a9ba97aa35f25055446af6c2f1b1171ac3ef
James (smitteh1) <thejamesash1@gmail.com>
2020-06-20 21:17:38 -0400

    make PayPalRedirect obj

--- PayPal-Wrapper/PayPalClient.php
+++ PayPal-Wrapper/PayPalClient.php
@@ -3,6 +3,19 @@
     
     require 'vendor/autoload.php';
 
+    use \PayPal\Api\Amount;
+    use \PayPal\Api\Details;
+    use \PayPal\Api\Item;
+    use \PayPal\Api\ItemList;
+    use \PayPal\Api\Payer;
+    use \PayPal\Api\Payment;
+    use \PayPal\Api\RedirectUrls;
+    use \PayPal\Api\Transaction;   
+
+    $configclass = new PayPalConfig();
+    $glbconfig = $configclass->returnConfig();
+    $defaultred = new PayPalRedirect($glbconfig->defaultRedirect, $glbconfig->defaultCancel);
+
     class PayPalConfig
     {	
     	private $config;
@@ -23,7 +36,6 @@
     {
     	private $config;
         private $context;
-        private $currency;
 
     	function __construct()
     	{
@@ -54,6 +66,18 @@
             return $this->config;
         }
 
+    }
+
+    class PayPalRedirect
+    {
+        public $return;
+        public $cancel;
+
+        function __construct($returnurl, $cancelurl)
+        {
+            $this->return = $returnurl;
+            $this->cancel = $cancelurl;
+        }
     }
 
     class PayPalItem
