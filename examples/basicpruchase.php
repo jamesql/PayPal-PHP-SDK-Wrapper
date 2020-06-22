5adab4090e948e448d50c9a62b48f0b7412108fd
James (smitteh1) <thejamesash1@gmail.com>
2020-06-22 17:30:00 -0400

    create example

---
+++ examples/basicpruchase.php
@@ -0,0 +1,25 @@
+<?php
+    namespace Demo;
+    require 'PayPal-Wrapper\PayPalClient.php';
+
+    // Create a PayPal 'client'
+    $client = new \PayPalWrapper\PayPalClient();
+
+    if (array_key_exists('button', $_POST))
+        purchase();
+
+    function purchase()
+    {
+        $order = $GLOBALS["client"]->CreateOrder(array(new \PayPalWrapper\PayPalItem("Some Sort of Item for 50$ (x3)", 3, 50)));
+        $GLOBALS["client"]->SendOrder($order, true);
+    }
+?>
+
+<html>
+    <body style="text-align:center;">
+        <h1 style="color:blue;"> PayPal SDK Wrapper Demo </h1>
+        <form method="post">
+            <input type="submit" name="button" value="button" />
+        </form>
+    </body>
+</html>
